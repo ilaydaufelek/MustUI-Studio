e4a8976412ec51211153dbb009c76663bacf6b13
ilaydaufelek <ilaydaufelek@gmail.com>
2026-01-02 00:35:21 +0300

    side updated

--- src/library/components/dropdown.tsx
+++ src/library/components/dropdown.tsx
@@ -8,16 +8,20 @@ interface DropdownContextType{
 interface DropdownMenuProps extends React.HTMLAttributes<HTMLDivElement> {
     children:React.ReactNode;
     className?:string
+     open?: boolean
+  onOpenChange?: (open: boolean) => void
 }
 
 interface DropdownMenuTriggerProps extends React.HTMLAttributes<HTMLDivElement> {
     children:React.ReactNode
     className?:string
+     asChild?: boolean
 }   
 
 interface DropdownMenuContentProps extends React.HTMLAttributes<HTMLDivElement> {
     children:React.ReactNode;
     className?:string
+    side?:"left" | "right"|"top" | "bottom"
 }
 
 interface DropdownMenuItemProps extends React.HTMLAttributes<HTMLDivElement> {
@@ -25,12 +29,41 @@ interface DropdownMenuItemProps extends React.HTMLAttributes<HTMLDivElement> {
     className?:string
 }
 
+const sides: Record<string, string> = {
+  bottom: "top-[100%] left-0",
+  top: "bottom-[100%] left-0",
+  right: "left-full top-0 mx-1",
+  left: "right-full top-0 mx-1",
+}
+
+const sideAnimation = {
+  left: {
+    open: "translate-x-0",
+    closed: "translate-x-0 opacity-0 pointer-events-none",
+  },
+  right: {
+    open: "translate-x-0",
+    closed: "translate-x-0 opacity-0 pointer-events-none",
+  },
+  top: {
+    open: "translate-y-0 ",
+    closed: " opacity-0 pointer-events-none",
+  },
+  bottom: {
+    open: "translate-y-0",
+    closed: " opacity-0 pointer-events-none",
+  },
+}
+
 const DropdownContext=createContext<DropdownContextType | null>(null)
 
-export const DropdownMenu=({children,className,...props}:DropdownMenuProps)=>{
-   const[open,setOpen]=useState(false)
+export const DropdownMenu=({children,className, open:controlledOpen, onOpenChange,...props}: DropdownMenuProps & React.HTMLAttributes<HTMLDivElement>)=>{
+    const [uncontrolledOpen, setUncontrolledOpen] = useState(false)
+
+  const open = controlledOpen ?? uncontrolledOpen
+  const setOpen = onOpenChange ?? setUncontrolledOpen
    return(
-    <DropdownContext.Provider  value={{open,setOpen}}>
+    <DropdownContext.Provider value={{open,setOpen}}>
        <div className={cn("relative inline-block  py-2",className)}{...props} >
          {children}
        </div>
@@ -39,33 +72,44 @@ export const DropdownMenu=({children,className,...props}:DropdownMenuProps)=>{
     
 }
 
-export const DropdownMenuTrigger=({children,className,...props}:DropdownMenuTriggerProps)=>{
+export const DropdownMenuTrigger=({children,className,asChild,...props}:DropdownMenuTriggerProps)=>{
  const ctx=useContext(DropdownContext)
  if (!ctx) throw new Error("DropdownTrigger must be used within DropdownMenu");
+ const handleClick=()=>{
+  ctx.setOpen(!ctx.open)
+ }
+
+
  return (
- <div className={cn("px-2 py-2 border flex items-center border-zinc-600/30 rounded-md cursor-default overflow-hidden  ",className)}
- onClick={()=>ctx?.setOpen(!ctx.open)}
+ <div className={cn("px-2 py-2 border flex items-center border-zinc-600/30 rounded-md cursor-default overflow-hidden font-semibold  ",className)}
+ onClick={handleClick}
   {...props} >
     {children}
+    
     </div>
   )
 }
 
-export const DropdownMenuContent=({children,className, ...props}:DropdownMenuContentProps)=>{
+export const DropdownMenuContent=({children,className,side="bottom" ,...props}:DropdownMenuContentProps)=>{
     
     const ctx = useContext(DropdownContext);
+    
+  if (!ctx) throw new Error("DropdownMenuContent must be used within DropdownMenu")
 
-
-  if (!ctx) throw new Error("DropdownMenuContent must be used within DropdownMenu");
-  if (!ctx.open) return null   
-    return (
-     <div 
-      className={cn("absolute top-full w-full    shadow-2xl rounded-md border border-zinc-700/25 dark:bg-zinc-950 bg-white ",className)} {...props} >
-        {children}
-      </div>
-    )
-
-   
+  return (
+    <div
+      className={cn(
+        "absolute min-w-full rounded-md border border-zinc-700/25 dark:bg-zinc-950 bg-white",
+        "transition-all duration-200 ease-out",
+        sides[side],
+         ctx.open ? sideAnimation[side].open : sideAnimation[side].closed,
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </div>
+  )
 }
 
 export const DropdownMenuItem=({children,className,...props}:DropdownMenuItemProps)=>{
@@ -73,7 +117,7 @@ export const DropdownMenuItem=({children,className,...props}:DropdownMenuItemPro
    return(
     <div 
     onClick={()=>ctx?.setOpen(false)}
-     className={cn("p-1 m-1 hover:bg-zinc-300/40 dark:hover:bg-zinc-800/30 cursor-default rounded-md text-sm font-medium  ",className)}{...props} >
+     className={cn("p-1 m-1   hover:bg-zinc-300/40 dark:hover:bg-zinc-800/30 cursor-default rounded-sm text-sm font-medium  ",className)}{...props} >
        {children}  
     </div>
    )
